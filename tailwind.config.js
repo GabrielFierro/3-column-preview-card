15f412e20fd48e64bc0f3ccddcd27f7ad86d6d8d
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-05-16 21:25:34 -0300

    add: custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./html"],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        // Primary
+        "bright-orange": "hsl(31, 77%, 52%)",
+        "dark-cyan": "hsl(184, 100%, 22%)",
+        "very-dark-cyan": "hsl(179, 100%, 13%)",
+        // Neutral
+        "transparent-white": "hsla(0, 0%, 100%, 0.75)", // Paragraphs
+        "very-light-gray": "hsl(0, 0%, 95%)" // Background, headings, buttons
+      },
+      fontFamily: {
+        title: ["Big Shoulders Display", "cursive"],
+        accent: ["Lexend Deca", "sans-serif"]
+      }
+    }
   },
   plugins: []
 };
